8bf308428a506232ec69d95e1fad563ad2619019
Nelli Aydinyan <aydinyanna@gmail.com>
2019-09-25 22:49:06 -0500

    Giggie's first_commit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
   // Handler when the DOM is fully loaded.
+function sayHello(obj){
+  alert("Harry Potter fans only");
+  debugger;
+  alert("I am giggles");
+
+};
+sayHello();
 });
