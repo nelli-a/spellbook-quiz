cc2a6a662c0ba161495bbd30e90fe1818f2a3def
Nelli Aydinyan <aydinyanna@gmail.com>
2020-03-01 15:04:24 -0600

    Question order

--- src/app.js
+++ src/app.js
@@ -1,26 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-  buttvar = document.querySelector("button.get-started")
-  buttvar.addEventListener("click", function(){
-    iv = document.querySelector(".introview")
-    iv.classList.add("hidden")
-    qv = document.querySelector(".questionview")
-    qv.classList.remove("hidden")
+  houseButton = document.querySelector("button.houses");
+  getStarted = document.querySelector("button.get-started");
+  getStarted.addEventListener("click", function(){
+    bgn = document.querySelector(".beginning");
+    bgn.classList.add("hidden");
+    qtn2 = document.querySelector(".questionview2");
+    qtn2.classList.remove("hidden");
   });
-  buttvar2 = document.querySelectorAll("button.answer")
-  var i;
-  for (i = 0; i < buttvar2.length; i++) {
-  selectAnswer = buttvar2[i];
-  selectAnswer.addEventListener("click", function(){
-    qv.classList.add("hidden")
-    qv2 = document.querySelector(".questionview2")
-    qv2.classList.remove("hidden")
-    });
-  };
+});
+
   // Handler when the DOM is fully loaded.
 
-});
 function setHouse(backgroundHouse){
   var backgroundTag = document.querySelector(".background");
   backgroundTag.classList.add(backgroundHouse);
+  hoPl = document.querySelector(".houseplace");
+  hoPl.classList.add("hidden");
+  bgn = document.querySelector(".beginning");
+  bgn.classList.remove("hidden");
   //backgroundTag.classList.remove("background");
 };
