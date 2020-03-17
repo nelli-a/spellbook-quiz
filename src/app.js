ba38dafe02972c035d736a648bf2ab4e209f9671
Nelli Aydinyan <aydinyanna@gmail.com>
2020-03-17 11:22:20 -0700

    Storing thechosen house

--- src/app.js
+++ src/app.js
@@ -1,17 +1,56 @@
+//Global Variables
+var spellQuestion = [];
+var effectAnswers = [];
+var finalTest;
+var housePlacement; //the house the user is in
+var questionLimit = 10; //the number of questions for the quiz
+var questions = [];
+
+//Script
 document.addEventListener("DOMContentLoaded", function() {
   houseButton = document.querySelector("button.houses");
   getStarted = document.querySelector("button.get-started");
   getStarted.addEventListener("click", function(){
     bgn = document.querySelector(".beginning");
     bgn.classList.add("hidden");
-    qtn2 = document.querySelector(".questionview2");
+    qtn2 = document.querySelector(".questionview");
     qtn2.classList.remove("hidden");
   });
-  document.getElementById('questionLabel').innerHTML = testQuestion();
+
+  callTheAPI()
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    finalTest = createQuestions(data);
+    console.log(finalTest);
+    var l = 0;
+    while (l < 10) {
+      console.log(finalTest[l].spell);
+      spellQuestion.push(finalTest[l].spell);
+      console.log(finalTest[l].effect);
+      effectAnswers.push(finalTest[l].wrong_effects);
+      effectAnswers.push(finalTest[l].effect);
+      document.getElementById('answer1').innerHTML = effectAnswers[1];
+      l++;
+    }
+    console.log(effectAnswers);
+    var qstnNumber = 0;
+    document.getElementById('questionLabel').innerHTML = finalTest[3].spell;
+  })
 })
 
-  // Handler when the DOM is fully loaded.
+function callTheAPI() {
+  return fetch('https://www.potterapi.com/v1/spells?key=$2a$10$6eUG7nLXoZX8bCzCYKjSVORXej7pITLm8RSnSc41c2PoB7YaO9.GG');
+}
 
+/*
+The function sets the background of the webpage based on the house the user chooses
+It then stores the value that was chosen in a global variable
+backgroundHouse is a CSS function that sets the background
+hoPl  is the var for the HTML houseplace View
+bgn is the var for the HTML beginning View
+*/
 function setHouse(backgroundHouse){
   var backgroundTag = document.querySelector(".background");
   backgroundTag.classList.add(backgroundHouse);
@@ -19,29 +58,49 @@ function setHouse(backgroundHouse){
   hoPl.classList.add("hidden");
   bgn = document.querySelector(".beginning");
   bgn.classList.remove("hidden");
-  //backgroundTag.classList.remove("background");
+  if (backgroundHouse == 'background-gryffindor'){
+    housePlacement = "Gryffindor";
+  }
+  else if (backgroundHouse == 'background-slytherin') {
+    housePlacement = "Slytherin";
+  }
+  else if (backgroundHouse == 'background-ravenclaw') {
+    housePlacement = "Ravenclaw";
+  }
+  else if (backgroundHouse == 'background-hufflepuff') {
+    housePlacement = "Hufflepuff";
+  }
+  //console.log(housePlacement);
 }
 
+/*
+Generates a random number within an object
+arrayObj is the object passed to the function
+randomIndex is the random integer generated
+*/
 function randomNumber(arrayObj){
   var randomIndex = Math.round(Math.random()*(arrayObj.length-1));
   return randomIndex;
 }
 
-var questionLimit = 10;
-var questions = [];
 
+/*
+createEffects is a function that creates an array with all the effects
+It is used to create randomised wrong effects in createQuestions function
+spellData is the data set that is passed to the function (PotterAPI)
+allEffects is an array with all the effects
+*/
 function createEffects(spellData){
-  var effectAll = [];
+  var allEffects = [];
   var e = 0;
   spellData.map(spellData => {
     var newEffect = spellData.effect;
-    effectAll.push(newEffect);
+    allEffects.push(newEffect);
   })
-  return effectAll;
+  return allEffects;
 }
-function testQuestion(){
-  return "test question worked";
-}
+
+//creates an array with all the questions, effects and wrong effects
 function createQuestions(spellData){
   var finalQuestion = {};
   var i = 0;
@@ -64,17 +123,3 @@ function createQuestions(spellData){
   };
   return finalQuestion;
 }
-
-fetch('https://www.potterapi.com/v1/spells?key=$2a$10$6eUG7nLXoZX8bCzCYKjSVORXej7pITLm8RSnSc41c2PoB7YaO9.GG')
-.then(response => {
-  return response.json();
-})
-.then(data => {
-  var finalTest = createQuestions(data);
-  var l = 0;
-  while (l < 10) {
-    console.log(finalTest[l].spell);
-
-    l++;
-  }
-})
